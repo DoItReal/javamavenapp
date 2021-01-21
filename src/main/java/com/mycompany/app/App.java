25ed76410460016f6ddf41df8901512e7a9d0299
Iliya Yankow <iliya.yankow@gmail.com>
2021-01-21 22:43:53 +0200

    Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,3 +1,6 @@
+
+
+
 package com.mycompany.app;
 
 /**
@@ -6,13 +9,15 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
     }
+
+
 
     private final String getMessage() {
         return message;
